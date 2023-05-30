ec72a34d32abcb55d8bfbbfa54015dbcdef4aabe
SimonDesc <pro.descombes@gmail.com>
2023-05-30 13:59:54 +0200

    Fix: case number in negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {abs(number)%10} ", end="")
+print(f"Last digit of {number} is ", end="")
 
-if (abs(number) % 10) > 5:
-    print(f"and is greater than 5")
-if (abs(number) % 10) == 0:
-    print(f"and is 0")
-if (abs(number) % 10) < 6 and (abs(number) % 10) != 0:
-    print(f"and is less than 6 and not 0")
+if (number < 0):
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+
+if (last_digit) > 5:
+    print(f"{last_digit} and is greater than 5")
+if (last_digit) == 0:
+    print(f"{last_digit} and is 0")
+if (last_digit) < 6 and (number % 10) >= 0:
+    print(f"{last_digit} and is less than 6 and not 0")
