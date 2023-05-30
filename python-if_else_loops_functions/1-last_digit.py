fa943ac1ca75b09b91f42c80c584a37819452110
SimonDesc <pro.descombes@gmail.com>
2023-05-30 13:41:37 +0200

    Fix: pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,7 @@ print(f"Last digit of {number} is {number%10} ", end="")
 
 if (number % 10) > 5:
     print(f"and is greater than 5")
-if (number % 10) < 6 and (number % 10) != 0:
-     print(f"and is less than 6 and not 0")
 if (number % 10) == 0:
     print(f"and is 0")
-
+if (number % 10) < 6 and (number % 10) != 0:
+    print(f"and is less than 6 and not 0")
