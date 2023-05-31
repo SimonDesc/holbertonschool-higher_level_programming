6c197749647016bf560b913713af83a7f369938b
SimonDesc <pro.descombes@gmail.com>
2023-05-31 10:59:51 +0200

    modify: range in for loop

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
+    for i in range(0, len(str)):
         if ord(str[i]) > 96 and ord(str[i]) < 123:
             num = 32
         else:
