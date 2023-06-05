f5263b3cd0881bf36b088d89f1438fc4a56c80af
SimonDesc <pro.descombes@gmail.com>
2023-06-05 18:20:42 +0200

    fix: pycode style

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,27 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
-    for i in range(len(matrix)): # pour chaque ligne i
-        for j in range(len(matrix[i])): # pour chaque colonne j dans la ligne i
-            print("{}".format(matrix[i][j]), end=" ") # imprime l'élément à la ligne i, colonne j
+    # pour chaque ligne i
+    for i in range(len(matrix)):
+        # pour chaque colonne j dans la ligne i
+        for j in range(len(matrix[i])):
+            # imprime l'élément à la ligne i, colonne j
+            print("{}".format(matrix[i][j]), end=" ")
         print("")
-
-
-
-"""
-M = [[3, 1, 5], [9, 8, -1], [10, 12, 2]]
-M[1] = [9, 8, -1]
-M[1][2] = -1
-A[0][-1] = 5
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-matrix[1] = 4 5 6
-
-
-
-"""
