d90605b574938d0028507318e45f8a5db8289a47
SimonDesc <pro.descombes@gmail.com>
2023-06-06 09:26:31 +0200

    Fix: decimal int print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
         # pour chaque colonne j dans la ligne i
         for j in range(len(matrix[i])):
             # imprime l'élément à la ligne i, colonne j
-            print("{}".format(matrix[i][j]), end="")
+            print("{:d}".format(matrix[i][j]), end="")
             if (j != len(matrix) - 1):
                 print("", end=" ")
         print("")
