13a877d4f9488ab25400877772a19547b0ec8e84
SimonDesc <pro.descombes@gmail.com>
2023-06-06 09:22:17 +0200

    Feat:  add condition if matrix is empty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return
+    if matrix == [[]]:
+        print()
     for i in range(len(matrix)):
         # pour chaque colonne j dans la ligne i
         for j in range(len(matrix[i])):
