58e6e0465775603efa047c3e1cb05d6dc8327667
SimonDesc <pro.descombes@gmail.com>
2023-06-06 15:22:53 +0200

    fix: modify the function - more pythonique

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
+    for element in my_list:
+        if element == search:
             new_list.append(replace)
         else:
-            new_list.append(my_list[i])
-    return(new_list)
+            new_list.append(element)
+    return new_list
