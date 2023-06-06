a9f70b1616feeef0123f43d4a8996d82df2d7ba1
SimonDesc <pro.descombes@gmail.com>
2023-06-06 09:18:51 +0200

    Fix: remove space EOF

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
-    # pour chaque ligne i
     for i in range(len(matrix)):
         # pour chaque colonne j dans la ligne i
         for j in range(len(matrix[i])):
             # imprime l'élément à la ligne i, colonne j
-            print("{}".format(matrix[i][j]), end=" ")
+            print("{}".format(matrix[i][j]), end="")
+            if (j != len(matrix) - 1):
+                print("", end=" ")
         print("")
