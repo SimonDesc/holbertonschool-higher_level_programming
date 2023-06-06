29273c2923b4264b0d37a6fa1250266b69cf07e7
SimonDesc <pro.descombes@gmail.com>
2023-06-06 09:29:07 +0200

    Fix loop

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
     for i in range(len(matrix)):
-        # pour chaque colonne j dans la ligne i
         for j in range(len(matrix[i])):
-            # imprime l'élément à la ligne i, colonne j
             print("{:d}".format(matrix[i][j]), end="")
-            if (j != len(matrix) - 1):
-                print("", end=" ")
+            if j != len(matrix[i]) - 1:
+                print(" ", end="")
             else:
                 print()
-        print("")
+    if matrix == [[]]:
+        print()
