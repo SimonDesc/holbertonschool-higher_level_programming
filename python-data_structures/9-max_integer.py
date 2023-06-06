a36c0f1effb9a5851c9c4a89c964d2ea0221ac61
SimonDesc <pro.descombes@gmail.com>
2023-06-06 13:29:42 +0200

    Fix: trying to fix a checker

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return
-    max = 0
-    current = 0
-    for i in range(len(my_list)):
-        current = my_list[i]
-        if (current > max):
+    max = my_list[0]
+    for i in range(1, len(my_list)):
+        if (my_list[i] > max):
             max = my_list[i]
     return max
