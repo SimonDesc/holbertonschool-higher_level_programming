149ee78693707edc68cce51359604613b3c3b789
SimonDesc <pro.descombes@gmail.com>
2023-06-07 14:22:16 +0200

    fix: add a check for validate the string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
+    if roman_string is None or not isinstance(roman_string, str):
+        return int_val
     for i in range(len(roman_string)):
         current_val = rom_val[roman_string[i]]
         if i > 0:
