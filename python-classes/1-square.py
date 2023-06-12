2de18a6d169d9bc8e0e2cfde7c4a904a585cfa7e
SimonDesc <pro.descombes@gmail.com>
2023-06-12 15:26:46 +0200

    fix: write doc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+First file with a class !
+"""
+
+
 class Square:
+    """
+    first class in Python
+    """
+
     def __init__(self, size):
         self._Square__size = size
-
-        
