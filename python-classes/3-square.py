4c1d006c3bb7028950cba84ac3543741e6c8ec7d
SimonDesc <pro.descombes@gmail.com>
2023-06-12 15:58:40 +0200

    feat: create a class

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+"""
+A class Square that defines a square
+"""
+
+
+class Square:
+    """
+    A class that check the size
+    and return the area
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    def area(self):
+        return (self._Square__size * self._Square__size)
