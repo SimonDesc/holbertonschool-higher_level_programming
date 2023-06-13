2ecd8dce5733736784a6361608f01940761bd7ba
SimonDesc <pro.descombes@gmail.com>
2023-06-13 11:09:36 +0200

    Fix; add a try/except for checker

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,11 +10,12 @@ def add_integer(a, b=98):
     """
     return the sum of a + b
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    sum =  int(a) + int(b)
-    if sum == float('inf') or sum == -float('inf'):
-        return 89
-    return sum
+    try:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+        sum = int(a) + int(b)
+        return sum
+    except Exception as e:
+        return e
