17aa3f14cfe376ed83cc03ff08b6c8417e4c5980
SimonDesc <pro.descombes@gmail.com>
2023-06-13 17:29:00 +0200

    fix: some refactoring and usage of strip()

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+function that prints a text with 2 new lines after
+each of these characters: ., ? and :
+"""
+
+
 def text_indentation(text):
+    """
+    function that prints a text
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     phrase = ""
