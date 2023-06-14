f51904647b276adf36ca59e3287762d8dd89e0d7
SimonDesc <pro.descombes@gmail.com>
2023-06-14 11:24:39 +0200

    Fix: refacto for checker

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-""" function divide all elem of a matrix """
+"""matrix_divided"""
 
 
-a = "matrix must be a matrix (list of lists) of integers/floats"
-
-
-def matrix_divided(matrix, div):
-    """ Random """
-    if matrix == [] or not isinstance(matrix, list):
-        raise TypeError(a)
-    if not isinstance(div, (int, float) or div is None):
+def matrix_divided(matrix=[], div=1):
+    """Return new matrix with value divided by @div"""
+    if not isinstance(div, (int, float)) or div is None:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    size = None
     new_matrix = []
-    new_baby = []
-    for row in matrix:
-        new_baby = []
-        for element in row:
-            try:
-                if div == float('inf'):
-                    new_baby.append(0.0)
-                else:
-                    new_baby.append(round(element / div, 2))
-            except:
-                if not isinstance(element, (int, float)):
-                    raise TypeError(a)
-        if len(new_baby) != len(matrix[0]):
+    j = 0
+    for i in matrix:
+        new_matrix.append([])
+        if size and size != len(i):
             raise TypeError("Each row of the matrix must have the same size")
-        new_matrix.append(new_baby)
-    return(new_matrix)
+        size = len(i)
+        for element in i:
+            if not isinstance(element, (int, float)):
+                raise TypeError(error)
+            new_matrix[j].append(round(element / div, 2))
+        j += 1
+    return new_matrix
