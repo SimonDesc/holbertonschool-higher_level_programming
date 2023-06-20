1bf09b561a7d7c84fb54249e6643d5c48ab21d97
SimonDesc <pro.descombes@gmail.com>
2023-06-20 13:56:12 +0200

    feat : create a class BaseGeometry with exception

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module with a class"""
+
+
+class BaseGeometry:
+    """Definition of a Class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
+
