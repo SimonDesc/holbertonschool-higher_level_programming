d2b890dd497cd8341c9366f9a14f6e6b975c1761
SimonDesc <pro.descombes@gmail.com>
2023-06-20 11:14:32 +0200

    feat: Write a class MyList that inherits from list:

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module with a class list"""
+
+
+class MyList(list):
+    """A class list that inherits from list"""
+    
+    def print_sorted(self):
+        """A public instance method that prints the list
+        in sorted ascending order
+
+        Args: nothing
+
+        Returns: nothing
+        """        
+        print(sorted(self))
