038c0f868c29f54b54e8c07f753a88f351c96b9c
SimonDesc <pro.descombes@gmail.com>
2023-06-21 11:16:34 +0200

    fix: return in the end ?

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" function that writes a string to a text file (UTF8) and 
-returns the number of characters written"""
+""" function that writes a string to a text file (UTF8)"""
 
 
 def write_file(filename="", text=""):
@@ -8,6 +7,6 @@ def write_file(filename="", text=""):
     try:
         with open(filename, "w", encoding="utf-8") as f:
             nb_characters = f.write(text)
-            return nb_characters
     except IOError:
         pass
+    return nb_characters
