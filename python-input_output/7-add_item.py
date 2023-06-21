dbd7168409696a0182559b4046a2349fde101668
SimonDesc <pro.descombes@gmail.com>
2023-06-21 14:50:50 +0200

    fix: somes comments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""write argv in a file"""
 import sys
 import os
 
@@ -8,14 +9,11 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 args = sys.argv
 filename = "add_item.json"
 
-# Vérifier si le fichier existe et lire son contenu
 if os.path.isfile(filename):
     my_list = load_from_json_file(filename)
 else:
     my_list = []
 
-# Ajouter les nouveaux éléments à la liste
 my_list.extend(args[1:])
 
-# Sauvegarder la liste dans le fichier
 save_to_json_file(my_list, filename)
