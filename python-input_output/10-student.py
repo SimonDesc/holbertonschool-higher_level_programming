2d5b0f480abc1a2b60cbf747a1f2619c3b5328f0
SimonDesc <pro.descombes@gmail.com>
2023-06-21 16:38:08 +0200

    feat: Write a class Student that defines a studen

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" class Student that defines a student"""
+
+
+class Student:
+    """Definition of a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            dict = {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+            return dict
