aff784099bf39d3aed89cbcb62911d9feeffcce1
SimonDesc <pro.descombes@gmail.com>
2023-06-21 11:14:59 +0200

    fix: return in a variable

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,7 @@ def write_file(filename="", text=""):
     """open and write"""
     try:
         with open(filename, "w", encoding="utf-8") as f:
-            return f.write(text)
+            nb_characters = f.write(text)
+            return nb_characters
     except IOError:
         pass
