564a5a2d4c0ea26a80224cb7a27c4ed0ab486218
SimonDesc <pro.descombes@gmail.com>
2023-06-23 15:27:52 +0200

    fix: trying to fix a checker

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -4,10 +4,10 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """search and add"""
-    with open(filename, "r+", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
         for i, line in enumerate(lines):
             if search_string in line:
                 lines.insert(i+1, new_string)
-    with open(filename, 'w') as f:  # 'w' signifie "write"
+    with open(filename, 'w', encoding="utf-8") as f:
         f.writelines(lines)
