bf72a61a84d1da7561c20a545e94fb1c39658325
SimonDesc <pro.descombes@gmail.com>
2023-06-23 10:18:33 +0200

    feat: create a function that block the creation of new attribut

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that block the creation of new attr"""
+class LockedClass:
+    def __init__(self):
+        super().__setattr__("first_name", "")
+
+    def __setattr__(self, attribute, value):
+        if attribute not in self.__dict__:
+            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
+
+        else:
+            super().__setattr__(attribute, value)
