fed18dac3c32502f9929c83a8723cfd38f59e1ed
SimonDesc <pro.descombes@gmail.com>
2023-06-23 11:15:28 +0200

    trying to fix that requirements

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -3,9 +3,21 @@
 
 
 def add_attribute(object, type, string):
-    """check if the attr is in dict or slots"""
+    """
+    The function adds a new attribute to an object if it is possible.
 
+    Args
+        obj (obj): The object to which the attribute needs to be added
+        attr_name (str): The name of the attribute that we want to add
+        attr_value (obj): The value of the attribute that we want to add
+
+    Returns: nothing
+
+    Raises:
+        TypeError: if it's not possible to add a new attribute to the object
+    """
     if not hasattr(object, '__dict__') or (hasattr(object, '__slots__')
                                            and type not in object.__slots__):
         raise TypeError("can't add new attribute")
-    setattr(object, type, string)
+    else:
+        setattr(object, type, string)
