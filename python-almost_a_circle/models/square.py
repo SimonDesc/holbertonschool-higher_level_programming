e849bbbe76be212d77ed7d53751f9692748b81ef
SimonDesc <pro.descombes@gmail.com>
2023-06-27 14:27:40 +0200

    feat: create update funct

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -30,3 +30,28 @@ class Square(Rectangle):
         else:
             self.width = value
             self.height = value
+            
+    def update(self, *args, **kwargs):
+        """update the args of rectangle"""
+        if not kwargs:
+            count = 0
+            for arg in args:
+                if count == 0:
+                    self.id = arg
+                if count == 1:
+                    self.size = arg
+                if count == 2:
+                    self.x = arg
+                if count == 3:
+                    self.y = arg
+                count += 1
+        else:
+            for cle, valeur in kwargs.items():
+                if cle == "id":
+                    self.id = valeur
+                if cle == "size":
+                    self.size = valeur
+                if cle == "x":
+                    self.x = valeur
+                if cle == "y":
+                    self.y = valeur
