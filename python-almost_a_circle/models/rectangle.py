dc92685f7aa93345c575d0e3020706bfb5f35c89
SimonDesc <pro.descombes@gmail.com>
2023-06-27 09:38:43 +0200

    feat: create a function that return the area

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
+
+    def area(self):
+        """Return the area"""
+        return self.__width * self.__height
