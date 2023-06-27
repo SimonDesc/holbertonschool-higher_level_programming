d6b57582c88d2c97dd2c23e9a37056027d072ceb
SimonDesc <pro.descombes@gmail.com>
2023-06-27 14:52:03 +0200

    fix: add if for empty list

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,4 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation"""
-        return json.dumps(list_dictionaries)
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
