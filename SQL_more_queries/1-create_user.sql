541920a7d7d24ae0110458aa4fb0ed2b9b555813
SimonDesc <pro.descombes@gmail.com>
2023-07-25 11:01:04 +0200

    with revoke

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -5,7 +5,13 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Accorde à 'user_0d_1' tous les privilèges sur toutes les bases de données et toutes les tables
-GRANT ALL TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- Actualise les privilèges pour s'assurer que les modifications prennent effet immédiatement
+FLUSH PRIVILEGES;
+
+-- Retire tous les privilèges de 'user_0d_1'
+REVOKE ALL PRIVILEGES ON *.* FROM 'user_0d_1'@'localhost';
+
+-- Actualise à nouveau les privilèges
 FLUSH PRIVILEGES;
