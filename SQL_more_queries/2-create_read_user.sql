e62da7386a5ce6c18b7780b9f08cbd70e7d41438
SimonDesc <pro.descombes@gmail.com>
2023-07-25 18:00:54 +0200

    trying grant select

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,6 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 -- Modifie le mot de passe de l'utilisateur 'user_0d_1' pour le mettre à 'user_0d_1_pwd'
 ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Accorde à 'user_0d_2' tous les privilèges sur toutes les bases de données et toutes les tables
-GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 -- Actualise les privilèges pour s'assurer que les modifications prennent effet immédiatement
 FLUSH PRIVILEGES;
