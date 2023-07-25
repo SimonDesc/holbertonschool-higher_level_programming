da467305c508d8bd394b41bf3bab698d9c9c730d
SimonDesc <pro.descombes@gmail.com>
2023-07-25 11:36:53 +0200

    without flush

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,5 +2,3 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
-
