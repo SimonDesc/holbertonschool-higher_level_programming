548a30ce8e85675ce600fc6ed9023f03b89fb02d
SimonDesc <pro.descombes@gmail.com>
2023-07-25 10:32:48 +0200

    checker en pls

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,11 @@
--- to create the mysql server user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Crée l'utilisateur 'user_0d_1' s'il n'existe pas déjà
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+-- Modifie le mot de passe de l'utilisateur 'user_0d_1' pour le mettre à 'user_0d_1_pwd'
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Accorde à 'user_0d_1' tous les privilèges sur toutes les bases de données et toutes les tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Actualise les privilèges pour s'assurer que les modifications prennent effet immédiatement
+FLUSH PRIVILEGES;
