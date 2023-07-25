a88a1328a62483712783e433ffdde86d80e1c442
SimonDesc <pro.descombes@gmail.com>
2023-07-25 10:22:37 +0200

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Crée l'utilisateur 'user_0d_1' s'il n'existe pas déjà
+-- Write a script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
