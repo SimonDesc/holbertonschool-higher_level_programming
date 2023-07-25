f762f3f97f3f36faf9c966f21cf8d70647333721
SimonDesc <pro.descombes@gmail.com>
2023-07-25 10:55:07 +0200

    test

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,8 +4,3 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 -- Modifie le mot de passe de l'utilisateur 'user_0d_1' pour le mettre à 'user_0d_1_pwd'
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Accorde à 'user_0d_1' tous les privilèges sur toutes les bases de données et toutes les tables
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Actualise les privilèges pour s'assurer que les modifications prennent effet immédiatement
-FLUSH PRIVILEGES;
