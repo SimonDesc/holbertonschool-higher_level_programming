6bdf78125cab22ea22183ed0bf0070acd9f4b18e
SimonDesc <pro.descombes@gmail.com>
2023-07-25 10:16:28 +0200

    add comments

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
--- Write a script that creates the MySQL server user user_0d_1. 
+-- Crée l'utilisateur 'user_0d_1' s'il n'existe pas déjà
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Accorde à 'user_0d_1' tous les privilèges sur toutes les bases de données et toutes les tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Actualise les privilèges pour s'assurer que les modifications prennent effet immédiatement
 FLUSH PRIVILEGES;
