7dbf9908c31f24b6d358ea4db29950dba649d6e1
SimonDesc <pro.descombes@gmail.com>
2023-07-25 10:19:20 +0200

    ADD WITH GRANT OPTION

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Accorde à 'user_0d_1' tous les privilèges sur toutes les bases de données et toutes les tables
-GRANT ALL PRIVILEGES TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Actualise les privilèges pour s'assurer que les modifications prennent effet immédiatement
 FLUSH PRIVILEGES;
