11f564a7a7ea894278a1d09d18fd6ece310e61a2
SimonDesc <pro.descombes@gmail.com>
2023-07-31 12:59:48 +0200

    add if __name__

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 """lists all states from database """
 
-import MySQLdb
-import sys
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
 
-user_db = sys.argv[1]
-passwd_db = sys.argv[2]
-name_db = sys.argv[3]
+    user_db = sys.argv[1]
+    passwd_db = sys.argv[2]
+    name_db = sys.argv[3]
 
-# Connexion à la BDD 
-db = MySQLdb.connect(host="localhost", user=user_db, passwd=passwd_db, db=name_db, port=3306)
+    # Connexion à la BDD 
+    db = MySQLdb.connect(host="localhost", user=user_db, passwd=passwd_db, db=name_db, port=3306)
 
-# Création d'un curseur
-cursor = db.cursor()
+    # Création d'un curseur
+    cursor = db.cursor()
 
-# Exécution de requêtes SQL
-cursor.execute("SELECT * FROM states")
+    # Exécution de requêtes SQL
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-# Récupération des résultats
-results = cursor.fetchall()
+    # Récupération des résultats
+    results = cursor.fetchall()
 
-# Affichage des résultats
-for element in results:
-    print(element);
+    # Affichage des résultats
+    for element in results:
+        print(element);
 
-# Femeture de la connexion
-db.close()
+    # Femeture de la connexion
+    db.close()
 
