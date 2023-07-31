77b7f469ff3eea8411d165b0cc62e9506915f1d1
SimonDesc <pro.descombes@gmail.com>
2023-07-31 13:05:06 +0200

    without comment for checker

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,23 +9,12 @@ if __name__ == "__main__":
     user_db = sys.argv[1]
     passwd_db = sys.argv[2]
     name_db = sys.argv[3]
-
-    # Connexion à la BDD 
+    
     db = MySQLdb.connect(host="localhost", user=user_db, passwd=passwd_db, db=name_db, port=3306)
-
-    # Création d'un curseur
     cursor = db.cursor()
-
-    # Exécution de requêtes SQL
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    # Récupération des résultats
     results = cursor.fetchall()
-
-    # Affichage des résultats
     for element in results:
         print(element);
-
-    # Femeture de la connexion
     db.close()
 
