043dc5dc8e890f3c6bd8ed9886fc2e73cb54761c
SimonDesc <pro.descombes@gmail.com>
2023-09-11 14:31:06 +0200

    .

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,11 +1,10 @@
-const toggleAddItem = document.getElementById("add_item");
-const myList = document.getElementsByClassName("my_list");
+const toggleAddItem = document.getElementById('add_item');
+const myList = document.querySelectorAll('.my_list');
 
-toggleAddItem.addEventListener("click", function () {
-  console.log(myList);
-  for (let i = 0; i < myList.length; i++) {
-    let li = document.createElement("li");
-    li.textContent = "Item";
-    myList[i].appendChild(li);
-  }
+toggleAddItem.addEventListener('click', () => {
+  myList.forEach(element => {
+    const li = document.createElement('li');
+    li.textContent = 'Item';
+    element.appendChild(li);
+  });
 });
