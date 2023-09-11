73ef8ef27b7eb272814e9de26a12d62451661202
SimonDesc <pro.descombes@gmail.com>
2023-09-11 12:05:50 +0200

    fix: fix toggle function

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,7 @@
+// Write a JavaScript script that adds the
+// class red to the header element when the user clicks on the tag with id red_header
 const titre = document.getElementById('red_header');
 const firstTitre = document.querySelector('header');
-toggleHeader.addEventListener('click', function () {
-  header.classList.add('red');
+titre.addEventListener('click', () => {
+  firstTitre.classList.toggle('red');
 });
